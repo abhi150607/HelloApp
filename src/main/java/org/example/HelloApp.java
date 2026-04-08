212a62bdaf7b06f479cc242dd9bd12bbf9ea7c47
Abhinav De <ad3621@srmist.edu.in>
2026-04-08 23:35:40 +0530

    [Abhinav] Added java file

---
+++ src/main/java/org/example/HelloApp.java
@@ -0,0 +1,19 @@
+package org.example;
+
+public class HelloApp {
+    public static void main(String[] args) {
+        // Step 1: Check if arguments are provided
+        if (args.length == 0) {
+            System.out.println("Hello, World!");
+        } else {
+            StringBuilder names = new StringBuilder();
+            for (String name : args) {
+                if (names.length() > 0) {
+                    names.append(", ");
+                }
+                names.append(name);
+            }
+            System.out.println("Hello, " + names.toString() + "!");
+        }
+    }
+}
