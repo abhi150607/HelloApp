f665f15dc23b6a8a4e1de979ae864a387179dd84
Abhinav De <ad3621@srmist.edu.in>
2026-04-08 23:34:50 +0530

    [Abhinav] Added java file

---
+++ src/main/java/org/example/HelloApp.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class HelloApp {
+    public static void main(String[] args) {
+        if (args.length == 0) {
+            System.out.println("Hello, World!");
+        } else {
+            System.out.println("Hello, " + String.join(", ", args) + "!");
+        }
+    }
+}
